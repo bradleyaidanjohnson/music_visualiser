b2ff15bb84d3348b760b1daeafbad0f63db45ab0
bradleyaidanjohnson <bradleyaidanjohnson@gmail.com>
2024-05-09 12:01:13 +0100

    working spec analyser, going horizontally with dynamic colour, also added notes to code

--- spectrum.js
+++ spectrum.js
@@ -1,18 +1,24 @@
-function Spectrum(){
-	this.name = "spectrum";
+function Spectrum() {
+  this.name = "spectrum";
 
-	this.draw = function(){
-		push();
-		var spectrum = fourier.analyze();
-		noStroke();
-		
-		fill(0,255,0)
-		for (var i = 0; i< spectrum.length; i++){
-			var x = map(i, 0, spectrum.length, 0, width);
-		    var h = -height + map(spectrum[i], 0, 255, height, 0);
-		    rect(x, height, width / spectrum.length, h );
-  		}
-	
-		pop();
-	};
+  this.draw = function () {
+    push();
+    // declare a variable to analse the audio spectrum
+    var spectrum = fourier.analyze();
+    noStroke();
+
+    // loop through all frequencies
+    for (var i = 0; i < spectrum.length; i++) {
+      // set the vertical heigh of the start of each bar dynmaically based on the spectrum and object height
+      var x = map(i, 0, spectrum.length, 0, height);
+      // set the width of each bar dynamically based on amplitude and object width
+      var w = map(spectrum[i], 0, 255, 0, width);
+      // set each bar to be more red/less green for higher amplitude
+      fill(spectrum[i], 255 - spectrum[i], 0);
+      // draw a rect using the set parameters
+      rect(0, x, w, width / spectrum.length);
+    }
+
+    pop();
+  };
 }
